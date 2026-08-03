c08668ce68b3401793ec380eabd7e082c60365f7
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2026-08-03 18:32:27 +0300

    test after

--- back-end/main.py
+++ back-end/main.py
@@ -26,4 +26,4 @@ app.add_middleware(
 
 @router.get("/")
 async def get_hello():
-    return {"message": "hello !", "key": {conf.secret_data}}
+    return {"message": "hello !", "key": {conf.secret_data}, "after deploy": "deploy"}
